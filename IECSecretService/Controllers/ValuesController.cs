4be5d9b197a6bbb4d5a405b9d8bd6d6f6608543b
Manuel Reinhart <manuel.reinhart@edu.campus02.at>
2019-12-04 20:21:15 +0100

    add several services for most want ted

---
+++ IECSecretService/Controllers/ValuesController.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Azure.Security.KeyVault.Secrets;
+using Azure.Identity;
+using Microsoft.Azure.Services.AppAuthentication;
+using Microsoft.Azure.KeyVault;
+using Microsoft.Azure.KeyVault.Models;
+
+namespace IECSecretService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+
+
+
+        // GET api/values
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<string>>> GetAsync()
+        {
+            var keyVaultUrl = "https://iegkeyvaultmar.vault.azure.net/";
+            var Message = "";
+            try
+            {
+                /* The next four lines of code show you how to use AppAuthentication library to fetch secrets from your key vault */
+                AzureServiceTokenProvider azureServiceTokenProvider = new AzureServiceTokenProvider();
+                KeyVaultClient keyVaultClient = new KeyVaultClient(new KeyVaultClient.AuthenticationCallback(azureServiceTokenProvider.KeyVaultTokenCallback));
+                var secret = await keyVaultClient.GetSecretAsync("https://iegkeyvaultmar.vault.azure.net/secrets/myPassword")
+                        .ConfigureAwait(false);
+                Message = secret.Value;
+            }
+            /* If you have throttling errors see this tutorial https://docs.microsoft.com/azure/key-vault/tutorial-net-create-vault-azure-web-app */
+            /// <exception cref="KeyVaultErrorException">
+            /// Thrown when the operation returned an invalid status code
+            /// </exception>
+            catch (KeyVaultErrorException keyVaultException)
+            {
+                Message = keyVaultException.Message;
+            }
+
+            //var client = new SecretClient(vaultUri: new Uri(keyVaultUrl), credential: new DefaultAzureCredential());
+
+            //var passowrd = client.GetSecret("myPassword").Value.Value;
+
+            return new string[] { "value1", "value2", Message };
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public ActionResult<string> Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/values/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ IEGWebhookService/Controllers/GithubController.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.WebHooks;
+using Newtonsoft.Json.Linq;
+
+namespace IEGWebhookService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GithubController : ControllerBase
+    {
+        [GitHubWebHook]
+        public IActionResult Github(string id, JObject data)
+        {
+            Console.WriteLine("GitHub Webhook Received" + data.ToString());
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [GitHubWebHook(EventName = "push", Id = "It")]
+        public IActionResult HandlerForItsPushes(string[] events, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [GitHubWebHook(Id = "It")]
+        public IActionResult HandlerForIt(string[] events, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [GitHubWebHook(EventName = "push")]
+        public IActionResult HandlerForPush(string id, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [GitHubWebHook]
+        public IActionResult GitHubHandler(string id, string @event, JObject data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+        [GeneralWebHook]
+        public IActionResult FallbackHandler(string receiverName, string id, string eventName)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            return Ok();
+        }
+
+
+    }
+}
